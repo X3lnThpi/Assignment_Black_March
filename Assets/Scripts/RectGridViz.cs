4c4cf74a2cb314696183c40cbab979a5a78482c0
Rahul Yadav <devmethyl@outlook.com>
2022-05-25 16:39:45 +0530

    Fallback to this, if raycast does not work

--- Assets/Scripts/RectGridViz.cs
+++ Assets/Scripts/RectGridViz.cs
@@ -74,7 +74,7 @@ public class RectGridViz : MonoBehaviour
 
             if (rectGridCell[i, j].isWalkable)
             {
-                neighbours.Add(rectGridCell[i, j]);
+                neighbours.Add(rectGridCell[i, j]); Debug.Log("Up Walkable");
             }
         }
 
@@ -166,6 +166,31 @@ public class RectGridViz : MonoBehaviour
 
     }
 
+    public void Display()
+    {
+        Vector2 rayPos = new Vector2(
+         Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+         Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
+        //
+        RaycastHit2D rayHit = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition));
+        Debug.Log("ray-hit" + " rayHit.transform.position");
+        Destroy(hit.transform.gameObject);
+
+        if (hit)
+        {
+            GameObject obj = hit.transform.gameObject;
+            RectGridCellViz sc = obj.GetComponent<RectGridCellViz>();
+            ToggleWalkable(sc);
+        }
+        Debug.Log("Display method calling");
+    }
+
+    public void ToggleWalkable(RectGridCellViz sc)
+    {
+        Debug.Log("Changed");
+    }
+
     private void Start()
     {
         Construct(row, column);
@@ -178,5 +203,9 @@ public class RectGridViz : MonoBehaviour
         //{
         //    Debug.Log("hit");
         //}
+        if (Input.GetMouseButtonDown(0))
+        {
+            Display();
+        }
     }
 }
